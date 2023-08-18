2c691ca08dff0d402f5c0be5b0f3777676f109f7
kirstenval03 <kirs0307@gmail.com>
2023-08-18 00:51:55 -0600

    Presentation page done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import { NavBar } from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Presentation } from './pages/Presentation';
+
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Presentation/>
      
     </div>
   );
---
+++ src/pages/Presentation.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react'
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { CaretRightSquareFill } from 'react-bootstrap-icons'
+import foto1 from "../assets/images/foto1.png"
+import foto2 from "../assets/images/foto2.png"
+
+
+export const Presentation = () => {
+  const [loopI, setLoopI] = useState(0);
+  const [deleting, setDeleting] = useState(false);
+  const txtAnimation = [ "Full Stack Developer", " Frontend Developer", "Backend Developer" ];
+  const[text, setText] = useState("");
+  const [delta, setDelta] = useState(200 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const period = 2000;
+
+  useEffect(() => {
+    let writer = setInterval(()  => {
+      writting();
+    },delta)
+    return () => {clearInterval(writer)};
+  }, [text])
+
+  const writting = () => {
+    let i = loopI % txtAnimation.length;
+    let fullText = txtAnimation[i];
+    let updatedText = deleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if(deleting){
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if(!deleting && updatedText === fullText){
+      setDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+
+    } else if (deleting && updatedText === '') {
+      setDeleting(false);
+      setLoopI(loopI + 1);
+      setIndex(1);
+      setDelta(250);
+
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+      
+    }
+
+  }
+
+  return (
+    <section className='presentation' id='home'>
+      <Container>
+        <Row className='align-items-center'>
+          <Col xs={12} md={6} xl={7} >
+            <span className='tagline'>
+            Welcome to my Portfolio!
+            </span>
+             <h1> {`Hi I'm Kirsten, a`}
+             <span className='wrap'> {text} </span> 
+             </h1>
+              <p>About me text...</p>
+              <button onClick={() => console.log("connect")}>Connect with me! <CaretRightSquareFill size={30}/> </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={foto1} alt='PresentationImg' className='presentationImg'/>
+          
+          </Col>
+
+        </Row>
+      </Container>
+
+    </section>
+  )
+}
+
